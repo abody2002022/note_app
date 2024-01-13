255a3d73043416a19eab1ddaa9b64578a8e7060c
body <am5753@fayoum.edu.eg>
2024-01-13 02:34:09 +0200

    create note model

---
+++ lib/views/widgets/Custom _text_fiels.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            decoration: InputDecoration(border: OutlineInputBorder()),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/add_model_bottom_sheet.dart
+++ lib/views/widgets/add_model_bottom_sheet.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/Custom%20_text_fiels.dart';
 
 class AddBottomSheet extends StatelessWidget {
   const AddBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [CustomTextField()],
+    );
   }
 }
