450fd2a664a914783d7a232012bca4c816fb5d94
body <am5753@fayoum.edu.eg>
2024-02-01 16:19:26 +0200

    finish_Bottom_sheet

--- lib/views/widgets/add_model_bottom_sheet.dart
+++ lib/views/widgets/add_model_bottom_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/Custom%20_text_fiels.dart';
+import 'package:note_app/views/widgets/constance.dart';
+
+import 'Custom_button.dart';
 
 class AddBottomSheet extends StatelessWidget {
   const AddBottomSheet({super.key});
@@ -8,22 +11,31 @@ class AddBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 34,
-          ),
-          CustomTextField(
-            hint: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'Content',
-            maxLines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 34,
+            ),
+            CustomTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
