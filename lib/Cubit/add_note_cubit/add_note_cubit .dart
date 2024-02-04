6109f929229eca30ef32ee146eb78168312fe477
body <am5753@fayoum.edu.eg>
2024-02-04 23:53:03 +0200

    add note states

--- lib/Cubit/add_note_cubit/add_note_cubit .dart
+++ lib/Cubit/add_note_cubit/add_note_cubit .dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit.dart';
 class AddNoteCubitState {}
 
 class AddNoteCubitInitial extends AddNoteCubitState {}
+
+class AddNoteCubitLoading extends AddNoteCubitState {}
+
+class AddNoteCubitSuccess extends AddNoteCubitState {}
+
+class AddNoteCubitFailure extends AddNoteCubitState {
+  final String errMessage;
+
+  AddNoteCubitFailure(this.errMessage);
+}
