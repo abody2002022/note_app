4c13ef4bc2f9bfba417db4775b75f32b612e9686
body <am5753@fayoum.edu.eg>
2024-02-09 05:33:18 +0200

    create fetch note methoud

--- lib/Cubit/cubit/notes_cubit.dart
+++ lib/Cubit/cubit/notes_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:note_app/Models/note_model.dart';
 
@@ -6,4 +7,15 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes() {
+    emit(NotesLoading());
+    try {
+      var notesbox = Hive.box<NoteModel>('note_box');
+
+      List<NoteModel> notes = notesbox.values.toList();
+      emit(NotesSucces(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/Cubit/cubit/notes_cubit.dart';
 import 'package:note_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
@@ -9,20 +11,24 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
-                context: context,
-                builder: (context) {
-                  return AddBottomSheet();
-                });
-          },
-          child: Icon(Icons.add),
-        ),
-        body: const NotesViewBody());
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  context: context,
+                  builder: (context) {
+                    return AddBottomSheet();
+                  });
+            },
+            child: Icon(Icons.add),
+          ),
+          body: const NotesViewBody()),
+    );
   }
 }
